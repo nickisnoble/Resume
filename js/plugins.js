6a0597a07608b5c63f96707db0054db1eb1c1f06
NickisNoble <nick@lanyapcreative.com>
2013-09-22 01:34:48 -0500

    - responsive awesome

--- js/plugins.js
+++ js/plugins.js
@@ -1,26 +1,13 @@
 $(window).on("scroll resize", function(){
-    // if($(window).width() >= 738) {
-            // if larger or equal
-             var pos = $('#date').offset(); //find #date
-			    $('article').each(function(){
-			        if(pos.top >= $(this).offset().top && pos.top <= $(this).next().offset().top){ //check #date is on article
-			            $('#date').html($(this).attr("data-date")); //grep from [data-date]
-			            return; //break the loop
-			        }
-			    });
-       //  } else {
-       //      var pos = $('header h1').offset(); //find header h1
-			    // $('article').each(function(){
-			    //     if(pos.top >= $(this).offset().top && pos.top <= $(this).next().offset().top){ //check header h1 is on article
-			    //         $('header h1').html($(this).attr("data-date")); //grep from [data-date]
-
-			    //         return; //break the loop
-			    //     }
-			    // });
-       //  }
+    var pos = $('#date').offset(); //find #date
+		$('article').each(function(){
+			if(pos.top >= $(this).offset().top && pos.top <= $(this).next().offset().top){ //check #date is on article
+				$('#date').html($(this).attr("data-date")); //grep from [data-date]
+				return; //break the loop
+			}
+		});
 });
 
 $(document).ready(function(){
-  $(window).trigger('scroll'); // init the value
+	$(window).trigger('scroll'); // init the value for scroll
 });
-
