89c82af991bb87ff8ea67745f45fb4a29b3d71f2
NickisNoble <nick@lanyapcreative.com>
2013-09-21 22:37:41 -0500

    - color social links

--- js/plugins.js
+++ js/plugins.js
@@ -1,8 +1,10 @@
 $(window).on("scroll resize", function(){
     var pos = $('#date').offset(); //find #date
     $('article').each(function(){
-        if(pos.top >= $(this).offset().top && pos.top <= $(this).next().offset().top){ //check #date is in between articles
-            $('#date').html($(this).attr("data-date")); //grep from [data-date]
+        if(pos.top >= $(this).offset().top && pos.top <= $(this).next().offset().top){ //check #date is on article
+         
+            $('#date').text($(this).attr("data-date")); //grep from [data-date]
+            
             return; //break the loop
         }
     });
